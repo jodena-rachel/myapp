17f52f62038d69054e52e4dac0e8d2da24c9720b
jodena-rachel <racheljodena127@gmail.com>
2026-09-22 08:17:35 +0300

    intial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::post('/store', [ContactController::class, 'store'])->name('contact.store');
+
+
+
+
+
+
